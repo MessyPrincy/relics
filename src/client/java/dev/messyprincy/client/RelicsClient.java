782993dc2f8efe36b775a7c14f0ddc0dbd85c600
MessyPrincy <alexan2004andw@gmail.com>
2026-08-10 09:45:30 -0400

    first commit

---
+++ src/client/java/dev/messyprincy/client/RelicsClient.java
@@ -0,0 +1,10 @@
+package dev.messyprincy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RelicsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
